3a1d08ca4b22ef5f53b5e5510b05d1a373a9f885
netoudi <netoudi@rocketmail.com>
2021-05-04 17:57:48 -0300

    Identificando temas e páginas no webpack config parte 3

--- frontend/config/webpack.config.js
+++ frontend/config/webpack.config.js
@@ -77,13 +77,15 @@ const hasJsxRuntime = (() => {
   }
 })();
 
-const keycloakTemplates = [];
+const themes = [];
 
 const themesDir = fs.readdirSync(paths.keycloakThemesPath, { withFileTypes: true })
   .filter((item) => item.isDirectory())
   .map((dir) => dir.name);
 
 for (const themeDir of themesDir) {
+  const keycloakTemplates = [];
+
   const pagesDir = fs.readdirSync(path.join(paths.keycloakThemesPath, themeDir))
     .filter((dir) => dir.endsWith('-pages'));
 
@@ -114,7 +116,16 @@ for (const themeDir of themesDir) {
       console.dir({ themesDir, pagesDir, templatesDir, ftlTemplates, keycloakTemplates }, { depth: 4 });
     }
   }
+
+  if (keycloakTemplates.length) {
+    themes.push({
+      themeDir: themeDir,
+      templates: keycloakTemplates
+    })
+  }
 }
+
+console.dir(themes, { depth: 4 });
 
 process.exit(0);
 
@@ -199,7 +210,7 @@ module.exports = function (webpackEnv) {
     return loaders;
   };
 
-  return {
+  return themes.map((theme) => ({
     mode: isEnvProduction ? 'production' : isEnvDevelopment && 'development',
     // Stop compilation early in production
     bail: isEnvProduction,
@@ -212,29 +223,15 @@ module.exports = function (webpackEnv) {
     // This means they will be the "root" imports that are included in JS bundle.
     entry:
       isEnvDevelopment && !shouldUseReactRefresh
-        ? [
-            // Include an alternative client for WebpackDevServer. A client's job is to
-            // connect to WebpackDevServer by a socket and get notified about changes.
-            // When you save a file, the client will either apply hot updates (in case
-            // of CSS changes), or refresh the page (in case of JS changes). When you
-            // make a syntax error, this client will display a syntax error overlay.
-            // Note: instead of the default WebpackDevServer client, we use a custom one
-            // to bring better experience for Create React App users. You can replace
-            // the line below with these two lines if you prefer the stock client:
-            //
-            // require.resolve('webpack-dev-server/client') + '?/',
-            // require.resolve('webpack/hot/dev-server'),
-            //
-            // When using the experimental react-refresh integration,
-            // the webpack plugin takes care of injecting the dev client for us.
-            webpackDevClientEntry,
-            // Finally, this is your app's code:
-            paths.appIndexJs,
-            // We include the app code last so that if there is a runtime error during
-            // initialization, it doesn't blow up the WebpackDevServer client, and
-            // changing JS code would still trigger a refresh.
+        ? theme.templates.reduce((previous, current) => {
+          previous[current.entry.chunk] = [
+            // webpackDevClientEntry
+            current.enty.path
           ]
-        : paths.appIndexJs,
+        }, {})
+        : theme.templates.reduce((previous, current) => {
+          return { ...previous, [current.entry.chunk]: current.entry.path };
+        }, {}),
     output: {
       // The build folder.
       path: isEnvProduction ? paths.appBuild : undefined,
@@ -794,5 +791,5 @@ module.exports = function (webpackEnv) {
     // Turn off performance processing because we utilize
     // our own hints via the FileSizeReporter
     performance: false,
-  };
+  }));
 };
