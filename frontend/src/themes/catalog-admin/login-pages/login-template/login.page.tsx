4879e940cae29679be331df535e6afcde817251f
netoudi <netoudi@rocketmail.com>
2021-08-31 09:19:35 -0300

    Entendendo estrutura básica da pagina de login

--- frontend/src/themes/catalog-admin/login-pages/login-template/login.page.tsx
+++ frontend/src/themes/catalog-admin/login-pages/login-template/login.page.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CssBaseline, MuiThemeProvider } from '@material-ui/core';
+import {
+  Box,
+  Button,
+  CssBaseline,
+  Grid,
+  Link,
+  MuiThemeProvider,
+  TextField,
+} from '@material-ui/core';
 
 import reportWebVitals from '../../../../reportWebVitals';
 import theme from '../../../../theme';
@@ -8,11 +16,71 @@ import Layout, { LayoutProps } from '../../components/Layout';
 
 declare const layoutProps: LayoutProps;
 
+export interface LoginPageProps {
+  //
+}
+
+const LoginPage: React.FunctionComponent<LoginPageProps> = (props) => {
+  return (
+    <Box padding={2}>
+      <Grid container spacing={3} justifyContent="space-evenly">
+        <Grid item>
+          <form action="" method="post">
+            <TextField
+              id="username"
+              name="username"
+              fullWidth
+              variant="outlined"
+              autoFocus
+              autoComplete="off"
+            />
+            <TextField
+              id="password"
+              name="password"
+              type="password"
+              fullWidth
+              variant="outlined"
+              autoFocus
+              autoComplete="off"
+              margin="normal"
+            />
+            <div>
+              <div>
+                <input type="checkbox" />
+                <Link href="#" color="secondary">
+                  &nbsp;
+                </Link>
+              </div>
+            </div>
+            <Grid container>
+              <Grid item xs={7}>
+                <div>
+                  <Link href="#" color="secondary">
+                    &nbsp;
+                  </Link>
+                </div>
+              </Grid>
+            </Grid>
+            <Grid item xs={5}>
+              <input type="hidden" id="id-hidden-input" name="credentialId" />
+              <Button type="submit" color="secondary" variant="contained">
+                Login
+              </Button>
+            </Grid>
+          </form>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
 ReactDOM.render(
   <React.StrictMode>
     <MuiThemeProvider theme={theme}>
       <CssBaseline />
-      <Layout {...layoutProps}>Login</Layout>
+      <Layout {...layoutProps}>
+        <LoginPage />
+      </Layout>
     </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
